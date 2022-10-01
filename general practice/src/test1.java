f754e46915328a6620c5d13275aa47b156cdf4f2
atuldandage <atuldandage1111@gmail.com>
2022-10-02 00:35:19 +0530

    deleted old data

--- general practice/src/test1.java
+++ general practice/src/test1.java
@@ -3,8 +3,7 @@ public class test1 {
 public static void main(String[] args) {
 	
 	
-	System.out.println("hello");
-	System.out.println("modified");
+	
 	System.out.println("this is 2nd modify");
 }
 }
